6a20929a297047c7292fbabc3c63bf304eaa190e
Kaili Hill <>
2023-03-09 12:58:53 -0800

    Adding some basic structure for dreamcast firmware

---
+++ Dreamcast/sw/mcu1/mcu1.c
@@ -0,0 +1,21 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Kaili Hill
+ */
+
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+// #include "hardware/structs/ssi.h"
+// #include "hardware/structs/ioqspi.h"
+// #include "FreeRTOS.h"
+// #include "task.h"
+// #include "mcu1.h"
+// #include "mcu2.h"
+// #include "qspi_helper.h"
+// #include "sdcard/internal_sd_card.h"
+
+int main(void) {
+    stdio_init_all();
+}
